e218b13bca6f11ec6fc9f5e55eabf3e083f43abf
Luis Da Costa <dacosta.le@gmail.com>
2017-04-07 13:48:33 -0400

    Model: properly persist

--- utils/doc2vec/model.py
+++ utils/doc2vec/model.py
@@ -5,17 +5,61 @@ from gensim.models import Doc2Vec
 import os
 
 from pathlib import Path
+from utils.Utils import append_to_git_root
+import hashlib
+import pickle
+
+from typing import List
 
 class Model:
     """Doc2Vec Model"""
 
-    models_directory = './doc2vec_models'
+    models_directory = append_to_git_root(what = './doc2vec_models', alternate_root=".")
+    params_file_name = 'params.pickle'
+    model_file_name = 'news.d2v'
+
     if not os.path.exists(models_directory):
         os.makedirs(models_directory)
 
+
+    def trained_model_exists(docs_bodies: pd.DataFrame) -> bool:
+        return False
+
     # ######### Private functions
-    def __file_full_path__(self) -> str:
-        return '{}/news_size{}.d2v'.format(self.models_directory, self.size)
+    def __ensure_directory_exists__(self):
+        d = self.persistence_directory()
+        if not os.path.exists(d):
+            os.makedirs(d)
+
+    def __file_full_path__(self, filename: str) -> str:
+        return '{}/{}'.format(self.persistence_directory(), filename)
+
+    def __save__(self) -> bool:
+        try:
+            self.__ensure_directory_exists__()
+            # Parameters
+            with open(self.__file_full_path__(self.params_file_name), 'wb') as f:
+                pickle.dump([self.window, self.min_count, self.size], f)
+            # Doc2Vec model
+            self.model.save(self.__file_full_path__(self.model_file_name))
+            print("Model saved in directory '{}'".format(self.persistence_directory()))
+            return True
+        except:
+            print("Model COULD NOT be saved in directory '{}'".format(self.persistence_directory()))
+            return False
+
+
+    def __load__(self) -> bool:
+        try:
+            # Parameters
+            with open(self.__file_full_path__(self.params_file_name), 'rb') as f:
+                self.window, self.min_count, self.size = pickle.load(f)
+            # Doc2Vec model
+            self.model = Doc2Vec.load(self.__file_full_path__(self.model_file_name))
+            return True
+        except:
+            # whatever the reason, this didn't work, so - whatever!
+            return False
 
     # #########
 
@@ -31,37 +75,53 @@ class Model:
         self.size = size
         self.docs_bodies = docs_bodies
         # labeled sentences
-        self.sentences = LabeledLineSentence(self.docs_bodies)
-        # Make the model and build the vocab
-        # Internal parameters set:
-        #   `workers` = use this many worker threads to train the model (=faster training with multicore machines).
-        #   `dm` defines the training algorithm. By default (`dm=1`), 'distributed memory' (PV-DM) is used.
-        #       Otherwise, `distributed bag of words` (PV-DBOW) is employed.
-        #   `hs` = if 1 (default), hierarchical sampling will be used for model training (else set to 0).
-        self.model = Doc2Vec(min_count = min_count, window = window, size = size, workers = 8, dm = 0, hs = 1)
-        self.model.build_vocab(self.sentences.to_array())
+        self.sentences = LabeledLineSentence(docs_bodies)
+        # let's try to load the model - in case I already trained with this corpus:
+        self.__load__()
 
-    def train(self):
+    def persistence_directory(self) -> str:
         """
-        Trains and saves resulting model.
-        :return: None
+        Directory where this Model should be saved.
         """
-        for epoch in range(20):
-            self.model.train(self.sentences.sentences_perm())
-        self.model.save(self.__file_full_path__())
+        sentences_as_strings = self.sentences.to_array()
 
-    def load_trained(self):
+        sentences_as_strings: List[str] = list(map(str, self.sentences.to_array())) # .sort()
+        sentences_as_strings.sort()
+        #
+        sentences_in_bodies: str = " ".join(sentences_as_strings)
+        hash_for_sentences = hashlib.sha1(sentences_in_bodies.encode()).hexdigest()
+        params_as_str = "w{}_mc{}_s{}".format(self.window, self.min_count, self.size)
+        return "/".join([self.models_directory, hash_for_sentences, params_as_str])
+
+    def train(self, window: int = 10, min_count: int = 2, size: int = 200, force: bool = False):
         """
-        Loads a trained model, if such thing exists. Trains a new one otherwise.
-        :return: 
+        :param window is the maximum distance between the predicted word and context words used for prediction
+        within a document.
+        :param min_count = ignore all words with total frequency lower than this.
+        :param size is the dimensionality of the feature vectors.
         """
-        try:
-            self.model = Doc2Vec.load(self.__file_full_path__())
-        except FileNotFoundError:
-            self.train()
+        self.window = window
+        self.min_count = min_count
+        self.size = size
+        if force or (not self.__load__()): # if I can load it, it means nothing has changed - so why train again?
+            # Make the model and build the vocab
+            # Internal parameters set:
+            #   `workers` = use this many worker threads to train the model (=faster training with multicore machines).
+            #   `dm` defines the training algorithm. By default (`dm=1`), 'distributed memory' (PV-DM) is used.
+            #       Otherwise, `distributed bag of words` (PV-DBOW) is employed.
+            #   `hs` = if 1 (default), hierarchical sampling will be used for model training (else set to 0).
+            self.model = Doc2Vec(min_count = min_count, window = window, size = size, workers = 8, dm = 0, hs = 1)
+            self.model.build_vocab(self.sentences.to_array())
+            # OK, good to train!
+            for epoch in range(20):
+                self.model.train(self.sentences.sentences_perm())
+            # and let's save - phew!
+            self.__save__()
 
     def is_trained(self)->bool:
         """
         Is there a file with a trained version?
         """
-        return Path(self.__file_full_path__()).is_file()
+        return Path(self.__file_full_path__(self.model_file_name)).is_file() \
+               and \
+               Path(self.__file_full_path__(self.params_file_name)).is_file()
--- utils/doc2vec/model_test.py
+++ utils/doc2vec/model_test.py
@@ -7,6 +7,18 @@ import pandas as pd
 class ModelsTest(unittest.TestCase):
 
 
+    def test_directory_is_constant(self):
+        df = pd.DataFrame({'Sentence': [randint(1, 9) for x in range(10)],
+                           'Body ID': [randint(1, 9) * 10 for x in range(10)],
+                           'Sentence ID': [randint(1, 9) * 100 for x in range(10)]})
+        # before training
+        m = Model(df)
+        m1 = Model(df)
+        self.assertEqual(m.persistence_directory(), m1.persistence_directory())
+        # after training
+        m.train(force = True)
+        self.assertEqual(m.persistence_directory(), m1.persistence_directory())
+
     def test_refute_malformed_bodies(self):
         with self.assertRaises(AssertionError):
             df = pd.DataFrame({'A': [randint(1, 9) for x in range(10)],
@@ -22,3 +34,8 @@ class ModelsTest(unittest.TestCase):
         m.train()
         # if it trained successfully, I should know!
         self.assertTrue(m.is_trained())
+        # if I create a new Model with same parameters it should remember that I was trained:
+        m1 = Model(df)
+        self.assertEqual(m.persistence_directory(), m1.persistence_directory())
+        self.assertTrue(m1.is_trained())
+
